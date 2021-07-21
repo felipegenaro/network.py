d2aaad37b737dd844a1ee78e57c8da3ee64be6a8
Felipe Corniani de Genaro <felipegenaro@outlook.com.br>
2021-07-21 15:22:29 -0300

    get location(ip) from url

---
+++ location_url.py
@@ -0,0 +1,12 @@
+import socket
+from ip2geotools.databases.noncommercial import DbIpCity
+
+url = input("Insert a URL: ")
+
+ip = socket.gethostbyname(url)
+response = DbIpCity.get(ip, api_key='free')
+
+print("IP: ", ip)
+print("City: ", response.city)
+print("Region: ", response.region)
+print("Country: ", response.country)
